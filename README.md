b20ae07d82e32a6c62f616b1bbdb12106fbf2334
Yubo Zhou <yubo@cse.yorku.ca>
2017-05-08 23:07:25 -0700

    working on my knn

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NeuralDecoding
 neural decoding project to decode brain neural activities
+
+
+# Some Results
+```bash
+Read 363 files in 2.320 secs
+num_files,std,alg_name,avg_precision,time
+363,0.193734578416,KNN 0.729572418616,52.406
+363,0.138302614063,Random Forest 0.680538594607,126.633
+363,0.139268680696,AdaBoost 0.676115370537,61.284
+```
--- decoding/test.py
+++ decoding/test.py
@@ -8,6 +8,7 @@ from multiprocessing import Pool
 import os, random, cProfile
 from collections import Counter
 from utils.knn import KNN
+from numpy import std
 import cProfile, pstats, io
 import numpy as np
 
@@ -22,33 +23,29 @@ The data processing is very slow, try improve the data processing part
 def preprocessing_load_test():
     return load_data()
 
-def end_to_end(inputs, scalar=0.1):
-    length = scalar
-    cut = int(len(inputs) * length)
-    inputs = inputs[:cut]
-    print(cut, end=',')
-    p = Pool(8)
-    p.map(run_model, inputs)
-    p.close()
-    p.join()
-    # print(ret)
 
-def end_to_end_verbose(inputs, scalar=0.1):
+
+def end_to_end_verbose(sessions, scalar=0.1):
     length = scalar
-    cut = int(len(inputs) * length)
-    print(cut)
+    num_files = int(len(sessions) * length)
+    print(num_files, end=',')
     p = Pool(8)
-    inputs = inputs[:cut]
-    ret = p.map(run_model, inputs)
+    sessions = sessions[:num_files]
+    ret = p.map(run_model, sessions)
     p.close()
     p.join()
     ret = [Counter(x) for x in ret]
+    # compute std of precision
+    results = [list(x.values())[0] for x in ret]
+    print(std(results), end=',')
     count = len(ret)
     all = Counter({})
     for each in ret:
         all = all + each
     for alg in all:
-        print(alg, 'precision:', all[alg]/count)
+        # print(alg, 'precision:', all[alg]/count)
+        print(alg, all[alg]/count, end=',')
+        # print(all[alg]/count,end=',')
 
         # for df in load_data():
     #     run_model(df)
@@ -57,14 +54,15 @@ def load_data():
     cur_dir = os.path.dirname(__file__)
     project_root = os.path.join(cur_dir, '..')
     data_folder = os.path.join(project_root, 'data')
-
+    # each session is a file, each file has 49 time points
     data_set = get_sessions(get_file_list(data_folder))
     return data_set
 
-def run_model(df, verbose=False):
+
+def run_model(session, verbose=False):
     # names = ['Decision Tree']
     # names = ['Nearest Neighbors', 'Decision Tree', "Random Forest", "AdaBoost"]
-    hyper_parameter = 3
+    hyper_parameter = 1
     if verbose:
         print(hyper_parameter, end=', ')
     names = ["Nearest Neighbors", "RBF SVM",
@@ -80,46 +78,16 @@ def run_model(df, verbose=False):
 
     classifier_pool = dict(zip(names, classifiers))
 
-
-    selected = ["KNN"]
-    # selected = ["Random Forest"]
+    # selected = ["KNN"]
+    selected = ["AdaBoost"] # "AdaBoost"
     # selected = ["Nearest Neighbors", "RBF SVM", "Decision Tree", "Random Forest", "AdaBoost", "KNN", "RadiusNeighbors"]
     results = dict(zip(selected, [0 for _ in range(len(selected))]))
     counter = 0
     # setup
     # names = ['Nearest Neighbors']
     classifiers = [classifier_pool[k] for k in selected]
-    # first_group_key = list(df.groups.keys())[0]
-    # first_group = df.get_group(first_group_key)
-    # test = [first_group for _ in range(100)]
-    # print(len(test[0]))
-    # for each in test:
-    #     data = each
-    #     inputs = []
-    #     labels = []
-    #     counter += 1
-    #     for record in data.iterrows():
-    #         dat = record[1]
-    #         inputs.append([dat['f1'], dat['f2']])
-    #         labels.append(int(dat['label']-1))
-    #     take_one_out = random.randint(0, len(inputs)-1)
-    #     # print(len(labels), take_one_out)
-    #     X_test = [inputs[take_one_out]]
-    #     y_test = [labels[take_one_out]]
-    #     # X_train = inputs[1:]
-    #     # y_train = labels[1:]
-    #     X_train = inputs[:take_one_out] + inputs[take_one_out:]
-    #     y_train = labels[:take_one_out] + labels[take_one_out:]
-    #     # print(each)
-    #     # iterate over classifiers
-    #     for name, clf in zip(selected, classifiers):
-    #         # print(name, clf)
-    #         clf.fit(X_train, y_train)
-    #         my_answer = clf.predict(X_test)
-    #         score = accuracy_score(my_answer, y_test)
-    #         results[name] += score
 
-    for each in df:
+    for each in session:
         data = each[1]
         inputs = []
         labels = []
@@ -164,16 +132,16 @@ s = io.StringIO()
 ps = pstats.Stats(pr, stream=s)
 # print(s.getvalue())
 ps.print_stats()
-print(get_time(s.getvalue()))
+print('Read', len(sessions), 'files', 'in', get_time(s.getvalue()), 'secs')
 
 
-print('num_files,time')
+print('num_files,std,alg_name,avg_precision,time')
 for i in range(1,2):
     pr.clear()
     pr.enable()
     # ... do something ...
-    # end_to_end(sessions, 1*(i/20))
-    end_to_end_verbose(sessions, 1*(i/20))
+    end_to_end_verbose(sessions, 1)
+    # end_to_end_verbose(sessions, 1*(i/20))
     pr.disable()
     s = io.StringIO()
     ps = pstats.Stats(pr, stream=s)
